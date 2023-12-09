84714ea91082efe91b79e90d504bb2e4737f0f45
Exargna <fox7games@mail.ru>
2023-12-09 18:37:30 +0600

    Задание выполнил. Не обращайте внимание на своеобразное наименование xD

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Phone.receiveCall("Люцифер");
+        Phone.receiveCall("Адам", 1);
+        Phone.receiveCall("Ева", 2);
+        Phone.country = "Китай";
+        Phone xiaomi = new Phone("13", "Xiaomi 13", 190);
+        System.out.println("\nНомер: " + xiaomi.number + "\nМодель: " +
+                xiaomi.model + "\nВес: " + xiaomi.weight + " грамм" + "\nСтрана Изготовитель: " + Phone.country);
+        Phone apple = new Phone("14", "Iphone 14 pro max", 230);
+        System.out.println("\nНомер: " + apple.number + "\nМодель: " +
+                apple.model + "\nВес: " + apple.weight + " грамм" + "\nСтрана Изготовитель: " + Phone.country);
+        Phone samsung = new Phone("23", "Galaxy S23", 185);
+        System.out.println("\nНомер: " + samsung.number + "\nМодель: " +
+                samsung.model + "\nВес: " + samsung.weight + " грамм" + "\nСтрана Изготовитель: " + Phone.country);
 
     }
 }
---
+++ Task 1/src/Phone.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Phone {
+    String number;
+    String model;
+    int weight;
+    String nameCaller;
+    static String country;
+
+    public static void receiveCall(String nameCaller) {
+        receiveCall(nameCaller, 0);
+    }
+
+    public static void receiveCall(String nameCaller, int numberCaller) {
+        System.out.println("Вам звонит абонент по имени " + nameCaller + " его номер " + (numberCaller > 0 ? numberCaller : "неизвестен"));
+    }
+
+    public Phone() {
+    }
+
+    public Phone(String number, String model, int weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+}
